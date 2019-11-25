f8d8d34e7efdf44f7ae0a2a1a0bd888f610ff531
Natalie <49326857+natyeo@users.noreply.github.com>
2019-11-25 16:56:04 +0000

    added equal and include tests

--- noteApp/testingFramework/assert.js
+++ noteApp/testingFramework/assert.js
@@ -1,41 +1,46 @@
 var expect = {
   isTrue: function(assertionToCheck) {
     if (!assertionToCheck) {
+      console.log("Test failed")
       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+    } else {
+      console.log("Test passed")
     }
   },
 
   toEqual: function(actual, expected) {
     if (expected !== actual) {
      console.log("Values are not equal")
-     console.log(`Expected Value ${expected} but got ${actual}`)
+     throw new Error(`Expected Value ${expected} but got ${actual}`)
     } else {
       console.log("Test passed")
     }
   },
 
-  toNotEqual: function(expected, actual) {
+  toNotEqual: function(actual, expected) {
     if (expected !== actual) {
-      logSuccess("Test passed")
+      console.log("Test passed")
     } else {
-      logFail("Values are equal")
-      console.log(`Expected \"${expected}\" not to equal \"${actual}\"`)
+      console.log("Values are equal")
+      throw new Error(`Expected \"${expected}\" not to equal \"${actual}\"`)
     }
   },
 
   toInclude: function(parent, child) {
     if(parent.includes(child)) {
-      logSuccess("Test Passed")
+      console.log("Test Passed")
     } else {
-      logFail(`Expected \"${child}"\ to be in \"${parent}\"`)
+      console.log("Test failed")
+      throw new Error(`Expected \"${child}"\ to be in \"${parent}\"`)
     }
   },
 
   toNotInclude: function(parent, child) {
     if(parent.includes(child)) {
-      logFail(`Expected \"${child}\" not to be in \"${parent}\"`)
+      console.log("Test failed")
+      throw new Error(`Expected \"${child}\" not to be in \"${parent}\"`)
     } else {
-      logSuccess("Test Passed")
+      console.log("Test Passed")
     }
   }
 }
--- noteApp/testingFramework/note-tests.js
+++ noteApp/testingFramework/note-tests.js
@@ -1,19 +1,31 @@
 
-  function testHelloWorld() {
+  function test1() {
     var note = new Note();
     expect.isTrue(note.helloWorld() === 'Hello World!');
   }
-  testHelloWorld();
 
-  function testHelloWorld() {
+  function test2() {
     var note = new Note();
     expect.toEqual(note.helloWorld(), "Hello World!")
   }
 
+  function test3() {
+    var note = new Note();
+    expect.toNotEqual("Hello", note.helloWorld())
+  }
 
-  // function testHelloWorld() {
-  //   var note = new Note();
-  //   equal.isEqual(note.helloWorld() === 'Hello');
-  // };
-  //
-  // testHelloWorld();
+  function test4() {
+    var note = new Note();
+    expect.toInclude(note.helloWorld(), "Hello")
+  }
+
+  function test5() {
+    var note = new Note();
+    expect.toNotInclude(note.helloWorld(), "blue")
+  }
+
+test1();
+test2();
+test3();
+test4();
+test5();
