578002de3259429c6854c6c850f08e7c9e8d9b9c
Natalie <49326857+natyeo@users.noreply.github.com>
2019-11-26 12:39:42 +0000

    title, form and button created

---
+++ noteApp/view.js
@@ -0,0 +1,36 @@
+// class View {
+//   constructor() {
+//   this.app = this.getElement('#root')
+//   this.form = this.createElement('form')
+//   this.input = this.createELement('input')
+//    // The title of the app
+//    this.title = this.createElement('h1')
+//    this.title.textContent = 'Notes'
+//   }
+//
+//   // Create an element with an optional CSS class
+//   createElement(tag, className) {
+//     const element = document.createElement(tag)
+//     if (className) element.classList.add(className)
+//
+//     return element
+//   }
+//
+//   getElement(selector) {
+//     const element = document.querySelector(selector)
+//
+//     return element
+// }
+// }
+
+document.getElementById("root").innerHTML = "Notes";
+
+var form = document.createElement('INPUT');
+form.setAttribute("type", "text");
+form.setAttribute("placeholder", "Enter note here");
+document.body.appendChild(form);
+
+var button = document.createElement("BUTTON");
+var text = document.createTextNode("Create Note");
+button.appendChild(text);
+document.body.appendChild(button);
