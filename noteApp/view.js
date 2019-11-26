588b66b8991333b418392abdbc110c44c98e4c8c
Arjun Roy <arjunroychoudhry4059@gmail.com>
2019-11-26 15:58:53 +0000

    Note Submitted

--- noteApp/view.js
+++ noteApp/view.js
@@ -28,6 +28,7 @@ document.getElementById("heading").innerHTML = "Notes";
 var form = document.createElement('INPUT');
 form.setAttribute("type", "text");
 form.setAttribute("placeholder", "Enter note here");
+form.id = "entry"
 document.body.appendChild(form);
 
 var button = document.createElement("BUTTON");
@@ -37,4 +38,5 @@ document.body.appendChild(button);
 note = new Note();
 button.addEventListener ("click", function() {
   note.create(form.value);
+  document.getElementById("entry").value = "Enter note here";
 })
