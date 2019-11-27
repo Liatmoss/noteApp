c441d0b1c117670475e61023d990a2887892f973
Natalie <49326857+natyeo@users.noreply.github.com>
2019-11-27 16:12:05 +0000

    refactored view

--- noteApp/view.js
+++ noteApp/view.js
@@ -1,18 +1,16 @@
 document.getElementById("heading").innerHTML = "Notes";
 
-var form = document.createElement('TEXTAREA');
-form.setAttribute("type", "text");
+var form = document.getElementById("entry");
 form.setAttribute("placeholder", "Enter note here");
-form.id = "entry"
-document.body.appendChild(form);
 
-var button = document.createElement("BUTTON");
+
+var button = document.getElementById("button");
 var text = document.createTextNode("Create Note");
 button.appendChild(text);
-document.body.appendChild(button);
+
 note = new Note();
 button.addEventListener ("click", function() {
   note.create(form.value);
-  document.getElementById("entry").placeholder = "Enter note here";
+  document.getElementById("entry").value = "Enter note here";
   document.getElementById("viewNotes").innerHTML = note.viewNotes(form.value);
 })
