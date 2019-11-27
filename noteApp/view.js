8624a6672b377c6798fc6f2d3fe2b0f7d49791da
Arjun Roy <arjunroychoudhry4059@gmail.com>
2019-11-27 15:24:34 +0000

    Notes Can Be Viewed on Page

--- noteApp/view.js
+++ noteApp/view.js
@@ -1,3 +1,5 @@
+// 'use strict';
+// import { Note } from "./note.js";
 // class View {
 //   constructor() {
 //   this.app = this.getElement('#root')
@@ -39,4 +41,16 @@ note = new Note();
 button.addEventListener ("click", function() {
   note.create(form.value);
   document.getElementById("entry").value = "Enter note here";
+  document.getElementById("viewNotes").innerHTML = note.viewNotes();
 })
+
+// note2 = new Note ();
+// var output = note.viewNotes();
+// document.body.onload = addElement;
+// function addElement () {
+// var newDiv = document.createElement("div");
+// var newContent = document.createTextNode(note.viewNotes());
+// newDiv.appendChild(newContent);
+// var currentDiv = document.getElementById("div1");
+// document.body.insertBefore(newDiv, currentDiv);
+// }
