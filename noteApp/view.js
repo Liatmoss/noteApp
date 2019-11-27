8f11f7cc82478c2cb522fe03042f5329b84b0fae
Arjun Roy <arjunroychoudhry4059@gmail.com>
2019-11-27 15:27:40 +0000

    Notes Can Be Viewed on Page UPDATE

--- noteApp/view.js
+++ noteApp/view.js
@@ -1,30 +1,3 @@
-// 'use strict';
-// import { Note } from "./note.js";
-// class View {
-//   constructor() {
-//   this.app = this.getElement('#root')
-//   this.form = this.createElement('form')
-//   this.input = this.createELement('input')
-//    // The title of the app
-//    this.title = this.createElement('h1')
-//    this.title.textContent = 'Notes'
-//   }
-//
-//   // Create an element with an optional CSS class
-//   createElement(tag, className) {
-//     const element = document.createElement(tag)
-//     if (className) element.classList.add(className)
-//
-//     return element
-//   }
-//
-//   getElement(selector) {
-//     const element = document.querySelector(selector)
-//
-//     return element
-// }
-// }
-
 document.getElementById("heading").innerHTML = "Notes";
 
 var form = document.createElement('INPUT');
@@ -43,14 +16,3 @@ button.addEventListener ("click", function() {
   document.getElementById("entry").value = "Enter note here";
   document.getElementById("viewNotes").innerHTML = note.viewNotes();
 })
-
-// note2 = new Note ();
-// var output = note.viewNotes();
-// document.body.onload = addElement;
-// function addElement () {
-// var newDiv = document.createElement("div");
-// var newContent = document.createTextNode(note.viewNotes());
-// newDiv.appendChild(newContent);
-// var currentDiv = document.getElementById("div1");
-// document.body.insertBefore(newDiv, currentDiv);
-// }
