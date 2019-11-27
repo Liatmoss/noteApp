60060e3ca2cad350b4d073540a3c30e2c0d0b794
liatmoss <liatmoss@hotmail.com>
2019-11-27 17:18:16 +0000

    made the interface easier to read

--- noteApp/view.js
+++ noteApp/view.js
@@ -2,9 +2,13 @@ document.getElementById("heading").innerHTML = "Notes";
 
 var form = document.getElementById("entry");
 form.setAttribute("placeholder", "Enter note here" );
+form.style.fontSize = '15px';
 
 
 var button = document.getElementById("button");
+button.style.width = '100px'
+button.style.height = '50px'
+button.style.fontSize = '15px'
 var text = document.createTextNode("Create Note");
 button.appendChild(text);
 
