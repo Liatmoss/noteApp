ebb72459c603e460723e2f25c9b06ebaa7ef20dc
Robert Woolley <rob@robertwoolley.co.uk>
2019-11-29 15:16:23 +0000

    Controller and view now split.

--- noteApp/view.js
+++ noteApp/view.js
@@ -11,26 +11,19 @@ button.style.fontSize = '15px'
 var text = document.createTextNode("Create Note");
 button.appendChild(text);
 
-note = new Note();
-button.addEventListener ("click", function() {
-  note.create(form.value);
-  var breakx = document.createElement("BR");
-  var noteIndex = note.noteIndex(form.value);
-  var noteLink = document.createElement("A");
-  var t = document.createTextNode(note.getTruncatedNote(noteIndex));
-  noteLink.setAttribute("id", noteIndex);
-  noteLink.appendChild(t);
-  var hash = "#" + noteIndex;
-  noteLink.setAttribute("href", hash);
-  document.getElementById("truncatedNotes").appendChild(noteLink);
-  document.getElementById("truncatedNotes").appendChild(breakx);
-  document.getElementById("entry").value = form.defaultValue;
+function renderSummary(truncatedtext, index)  {
+    var breakx = document.createElement("BR");
+    var noteLink = document.createElement("A");
+    var t = document.createTextNode(truncatedtext);
+    noteLink.setAttribute("id", index);
+    noteLink.appendChild(t);
+    var hash = "#" + index;
+    noteLink.setAttribute("href", hash);
+    document.getElementById("truncatedNotes").appendChild(noteLink);
+    document.getElementById("truncatedNotes").appendChild(breakx);
+    document.getElementById("entry").value = form.defaultValue;
+}
 
-  window.addEventListener("hashchange", myFunction);
-    function myFunction() {
-    var fullNote = document.getElementById(noteIndex);
-    fullNote.addEventListener("click", function(clickEvent) {
-      document.getElementById("fullNote").innerHTML = note.fullNote(noteIndex);
-    });
-  };
-});
+function renderText(outputText) {
+  document.getElementById("fullNote").innerHTML = outputText;
+};
